e21bdc287ce537bfda9ee05f91a1aa8e239d35f1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-12-29 13:29:02 +0100

    add main elements to the view

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,24 @@ function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
   console.log(wordToGuess);
-  return <h1>Hi!</h1>
+  return <div style={{
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center"
+  }}>
+    <div style={{ fontSize: "2rem", textAlign: "center" }}>
+      Lose
+      Win
+    </div>
+    {/* <HangmanDrawing />
+    <HangmanWord />
+    <Keyboard /> */}
+  </div>
 }
 export default App
