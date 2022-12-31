2ee51c6aad1c2c7193197040e354d3fb3f653294
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-12-31 11:16:41 +0100

    add letters to Keyboard module

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -10,7 +10,7 @@ export function HangmanWord(){
         fontFamily: "monospace"}}>
     
         {word.split("").map((letter, index) => (
-        <span style={{borderBottom: ".1em solid black"}}>
+        <span style={{borderBottom: ".1em solid black"}} key={index}>
             <span style={{visibility: guessedLetters.includes(letter) ? "visible" : "hidden"}}> {letter}</span>
         </span>
     ))}
