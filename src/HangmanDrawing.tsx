efda931d180b0d839d1ac63a2378eee96ce64b11
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-01-02 09:15:42 +0100

    add logic to show body parts when letter is wrong

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -73,18 +73,15 @@ const LEFT_LEG = (
     }}/>
 )
 
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+
 type HangmanDrawingProps = {
     numberOfGuesses: number;
 }
 
 export function HangmanDrawing({numberOfGuesses} : HangmanDrawingProps){
     return <div style={{position: "relative"}}>
-        {HEAD}
-        {BODY}
-        {LEFT_ARM}
-        {RIGHT_ARM}
-        {LEFT_LEG}
-        {RIGHT_LEG}
+      {BODY_PARTS.slice(0, numberOfGuesses)}
         <div style={{
             height: "50px",
             width: "10px",
